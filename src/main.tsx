2e4f842e28785a67df57c18a08ab0dbfe4697a3c
XuaoYuRun <XuaoYuRun@users.noreply.github.com>
2026-09-24 22:41:10 +0800

    feat: extend wallpaper glass across UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@fontsource-variable/manrope";
 import "./styles.css";
+import "./wallpaper-glass.css";
 import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
